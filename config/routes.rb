4861dbf62e0e924585e875c709ff7eabb494f9de
Esteban Wilches <fewilchesc@gmail.com>
2020-04-13 18:46:27 -0500

    CRUD V1 + DOCKER - VERSION PRESENTADA EN VIDEO PROTOTIPO 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :vehicle_types
   resources :service_types
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'vehicles#index', as: 'root'
+  root 'services#index', as: 'root'
   get '/vehicle_types/:id/vehicles', to: 'vehicles#vehiclesByType', as: 'show_vehicles_by_type'
   get '/vehicle_types/:id/services', to: 'services#servicesByVehicleType', as: 'show_services_by_vehicle_type'
   get '/service_types/:id/services', to: 'services#servicesByType', as: 'show_services_by_type'
